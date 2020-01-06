2dfc209595773e0a627e589c4e4b14db649c9d85
Bryan A. Alvarado Villalobos <bryan.amazon.alvarado@gmail.com>
2020-01-06 11:00:50 -0500

    Commit branch developer Inicial

---
+++ src/test/java/com/slayer/portafolio/mysql/ubigeo/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slayer.portafolio.mysql.ubigeo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
